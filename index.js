77faecee65b13c0a4a556497d1ed92240760103d
diepnhpvi <diepnhpvi@gmail.com>
2016-08-30 00:04:30 +0700

    from 1337 port to 80 port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'mPyeByykpRn5LkfQAwrZCvTR38SwqsG7KLHVCykM',
   masterKey: process.env.MASTER_KEY || 'mURND1KM48hBqPnF73ktYqlgXjU0ObcnfHnDVLyp', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:80/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
